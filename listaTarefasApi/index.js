cfdc65ca243c3117c15d38d0d986caec9b87bf5e
Lincon Agrizzi <lincon_ac@hotmail.com>
2023-06-10 20:31:29 -0300

    finish code
    In this project I did the manipulation of cookies, also includes creation of server, model and database. Frontend to consume API with routes and controllers.
    
    It is a simple system for registering tasks.

---
+++ listaTarefasApi/index.js
@@ -0,0 +1,23 @@
+//BIBLIOTECAS/MODULOS UTILIZADOS
+const database = require("./db/db");
+const routes = require("./routes/routes");
+const express = require("express");
+const app = express();
+//const usuarioController = require("./controller/usuarioController");
+//CODIFICAÇÃO UTILIZAÇÃO DO JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//MODELS
+const Usuario = require("./model/usuarioModel");
+const Tarefa = require("./model/tarefaModel");
+//SINCRONISMO COM O BANCO DE DADOS
+try {
+ database.sync().then(() => {
+ })
+}
+catch(erro) {
+ console.log("Houve uma falha ao sincronizar com o banco de dados. ", erro);
+};
+//ROTA PRINCIPAL
+app.use("/",routes);
+app.listen(3000);
